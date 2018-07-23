e41090803cfa5e8fe792f52f292dec77c8c5f80a
liyancode <liyan.code@gmail.com>
2018-07-23 09:53:37 +0800

    ask prices srvc

--- src/js/_services/order.service.js
+++ src/js/_services/order.service.js
@@ -1,7 +1,11 @@
 import { authHeader } from '../_helpers/auth-header';
 import {message} from 'antd'
 export const orderService = {
-    getOrders
+    getOrders,
+    addNewAskPrice,
+    getAskPricesByUserName,
+    getAskPriceById,
+    updateAskPrice
 };
 
 function getOrders() {
@@ -12,6 +16,41 @@ function getOrders() {
 
     return fetch(`/api/orders`, requestOptions).then(handleResponse);
 }
+function addNewAskPrice(ask_price) {
+    const requestOptions = {
+        method: 'Post',
+        headers: authHeader(),
+        body:JSON.stringify(ask_price)
+    };
+
+    return fetch(`/api/orders/ask_prices/ask_price`, requestOptions).then(handleResponse);
+}
+
+function getAskPricesByUserName() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`/api/orders/ask_prices/by_user_name`, requestOptions).then(handleResponse);
+}
+function getAskPriceById(ask_price_id) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`/api/orders/ask_prices/ask_price/`+ask_price_id, requestOptions).then(handleResponse);
+}
+function updateAskPrice(ask_price) {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`/api/orders/ask_prices/by_user_name`, requestOptions).then(handleResponse);
+}
+
 
 function handleResponse(response) {
     return response.text().then(text => {
