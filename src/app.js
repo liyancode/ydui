e262a6085d10468a5a27068b00b4794ccaf6231d
liyancode <liyan.code@gmail.com>
2018-07-28 21:16:18 +0800

    zh-cn

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {HashRouter, Router, Route,Switch} from 'react-router-dom';
+import { LocaleProvider } from 'antd';
+import zhCN from 'antd/lib/locale-provider/zh_CN';
+
 
 import './3rdparty/bootstrap-3.3.7-dist/css/bootstrap.min.css'
 
@@ -14,12 +17,14 @@ import Login from './js/login'
 import PageHome from "./js/pageComponents/pageHome"
 import PageFin from "./js/pageComponents/pageFin";
 import PageCRM from "./js/pageComponents/pageCRM";
-import PageOrder from "./js/pageComponents/pageOrder";
 import PageWereHouse from "./js/pageComponents/pageWereHouse";
 import PageHR from "./js/pageComponents/pageHR";
 
 import './css/style.css';
 import PageProduct from "./js/pageComponents/pageProduct";
+import PageAskPrice from "./js/pageComponents/order/pageAskprice";
+import PageContract from "./js/pageComponents/order/pageContract";
+import PageOrder from "./js/pageComponents/order/pageOrder";
 
 class App extends React.Component {
     constructor(props) {
@@ -48,6 +53,8 @@ class App extends React.Component {
                     <PrivateRoute exact path="/" component={PageHome}/>
                     <PrivateRoute path="/hr" component={PageHR}/>
                     <PrivateRoute path="/crm" component={PageCRM}/>
+                    <PrivateRoute path="/ask_price" component={PageAskPrice}/>
+                    <PrivateRoute path="/contract" component={PageContract}/>
                     <PrivateRoute path="/order" component={PageOrder}/>
                     <PrivateRoute path="/product" component={PageProduct}/>
                     <PrivateRoute path="/fin" component={PageFin}/>
@@ -59,4 +66,4 @@ class App extends React.Component {
     }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<LocaleProvider locale={zhCN}><App/></LocaleProvider>, document.getElementById('app'));
