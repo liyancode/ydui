b99a94deb259dbf8c56245fddb79d8ab4ac8ca77
liyancode <liyan.code@gmail.com>
2018-08-09 22:37:54 +0800

    authority hash function

--- src/js/_helpers/authorityConstants.js
+++ src/js/_helpers/authorityConstants.js
@@ -1,3 +1,10 @@
-export const authorityConstants={
+export function authorityHash() {
+    // return authorization header with jwt token
+    let user = JSON.parse(localStorage.getItem('user'));
 
+    if (user && user.authorityHash) {
+        return user.authorityHash;
+    } else {
+        return {};
+    }
 }
