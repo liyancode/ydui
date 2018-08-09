23e65d5d0d2820b798e79ea9a773c985be8907fe
liyancode <liyan.code@gmail.com>
2018-08-09 22:37:16 +0800

    authority filter

--- src/js/_components/compnSider.js
+++ src/js/_components/compnSider.js
@@ -1,11 +1,131 @@
 import React from 'react';
-import {Icon,Layout, Menu} from 'antd';
+import {Icon, Layout, Menu} from 'antd';
+
 const {Sider} = Layout;
 import {Link} from 'react-router-dom'
 
-const { SubMenu } = Menu;
+const {SubMenu} = Menu;
 
-const CompnSider=(props)=> {
+const CompnSider = (props) => {
+
+    // 'r' 'rw' 'n'
+    // user_authority={'hr':'r','order':'rw',...}
+    const user_authority = props.user_authority;
+    let menu_items = [];
+
+    let hr_autho = user_authority['hr'];
+    if (hr_autho === 'r') {
+        menu_items.push(
+            <Menu.Item key="1">
+                <Link to='/hr' replace>
+                    <Icon type="user"/>
+                    <span>员工档案</span>
+                </Link>
+            </Menu.Item>
+        )
+    } else if (hr_autho === 'rw') {
+
+    }
+
+    let order_autho = user_authority['order']
+    if (order_autho === 'r') {
+
+    } else if (order_autho === 'rw') {
+        menu_items.push(
+            <SubMenu
+                key="order_m"
+                title={<span><Icon type="shopping-cart"/><span>订单管理</span></span>}
+            >
+
+                <Menu.Item key="order_page">
+                    <Link to='/order' replace>
+                        <Icon type="shopping-cart"/>
+                        <span>订单</span>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="contract_page">
+                    <Link to='/contract' replace>
+                        <Icon type="book"/>
+                        <span>合同</span>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="ask_price_page">
+                    <Link to='/ask_price' replace>
+                        <Icon type="calculator"/>
+                        <span>询价</span>
+                    </Link>
+                </Menu.Item>
+            </SubMenu>
+        )
+    }
+
+    let crm_autho = user_authority['crm']
+    if (crm_autho === 'r') {
+
+    } else if (crm_autho === 'rw') {
+        menu_items.push(
+            <Menu.Item key="3">
+                <Link to='/crm' replace>
+                    <Icon type="contacts"/>
+                    <span>客户管理</span>
+                </Link>
+            </Menu.Item>
+        )
+    }
+
+    let fin_autho = user_authority['fin']
+    if (fin_autho === 'r') {
+
+    } else if (fin_autho === 'rw') {
+        menu_items.push(
+            <Menu.Item key="4">
+                <Link to='/fin' replace>
+                    <Icon type="pay-circle-o"/>
+                    <span>财务审批</span>
+                </Link>
+            </Menu.Item>
+        )
+    }
+
+    let product_autho = user_authority['product']
+    if (product_autho === 'r') {
+
+    } else if (product_autho === 'rw') {
+        menu_items.push(
+            <SubMenu
+                key="product_m"
+                title={<span><Icon type="shop"/><span>产品管理</span></span>}
+            >
+                <Menu.Item key="product_mianliao_page">
+                    <Link to='/product_mianliao' replace>
+                        <Icon type="shopping-cart"/>
+                        <span>面料</span>
+                    </Link>
+                </Menu.Item>
+                <Menu.Item key="product_fengguan_page">
+                    <Link to='/product_fengguan' replace>
+                        <Icon type="book"/>
+                        <span>风管</span>
+                    </Link>
+                </Menu.Item>
+            </SubMenu>
+        )
+    }
+
+    let warehouse_autho = user_authority['product']
+    if (warehouse_autho === 'r') {
+
+    } else if (warehouse_autho === 'rw') {
+        menu_items.push(
+            <Menu.Item key="6">
+                <Link to='/warehouse' replace>
+                    <Icon type="database"/>
+                    <span>库存管理</span>
+                </Link>
+            </Menu.Item>
+        )
+    }
+
     return (
         <Sider
             breakpoint="lg"
@@ -37,7 +157,7 @@ const CompnSider=(props)=> {
                 </Menu.Item>
                 <SubMenu
                     key="order_m"
-                    title={<span><Icon type="shopping-cart" /><span>订单管理</span></span>}
+                    title={<span><Icon type="shopping-cart"/><span>订单管理</span></span>}
                 >
 
                     <Menu.Item key="order_page">
@@ -48,13 +168,13 @@ const CompnSider=(props)=> {
                     </Menu.Item>
                     <Menu.Item key="contract_page">
                         <Link to='/contract' replace>
-                            <Icon type="book" />
+                            <Icon type="book"/>
                             <span>合同</span>
                         </Link>
                     </Menu.Item>
                     <Menu.Item key="ask_price_page">
                         <Link to='/ask_price' replace>
-                            <Icon type="calculator" />
+                            <Icon type="calculator"/>
                             <span>询价</span>
                         </Link>
                     </Menu.Item>
@@ -73,7 +193,7 @@ const CompnSider=(props)=> {
                 </Menu.Item>
                 <SubMenu
                     key="product_m"
-                    title={<span><Icon type="shop" /><span>产品管理</span></span>}
+                    title={<span><Icon type="shop"/><span>产品管理</span></span>}
                 >
                     <Menu.Item key="product_mianliao_page">
                         <Link to='/product_mianliao' replace>
@@ -83,13 +203,13 @@ const CompnSider=(props)=> {
                     </Menu.Item>
                     <Menu.Item key="product_fengguan_page">
                         <Link to='/product_fengguan' replace>
-                            <Icon type="book" />
+                            <Icon type="book"/>
                             <span>风管</span>
                         </Link>
                     </Menu.Item>
                 </SubMenu>
                 <Menu.Item key="6">
-                    <Link to='/werehouse' replace>
+                    <Link to='/warehouse' replace>
                         <Icon type="database"/>
                         <span>库存管理</span>
                     </Link>
