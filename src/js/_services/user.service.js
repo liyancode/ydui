c32776da15a9649628bd7fdcf01afcae3d3b1f93
liyancode <liyan.code@gmail.com>
2018-08-27 08:45:26 +0800

    user service

--- src/js/_services/user.service.js
+++ src/js/_services/user.service.js
@@ -1,23 +1,21 @@
-import { authHeader } from '../_helpers/auth-header';
+import { authHeader } from '../_helpers/authHeader';
 import {message} from 'antd'
 export const userService = {
     login,
     logout,
     // register,
+    addUser,
     getAll,
-    // getById,
-    getUserByUsername
+    updateUser,
+    getUserByUsername,
     // update,
-    // delete: _delete
+    deleteUser
 };
 
 function login(username, password) {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/x-www-form-urlencoded','Accept':'application/json' },
-        // headers: { 'Content-Type': 'application/json' },
-        // credentials: 'same-origin',
-        // mode: 'no-cors',
         body: "username="+username+"&password="+password
     };
 
@@ -25,22 +23,50 @@ function login(username, password) {
         .then(handleResponse)
         .then(user => {
             // login successful if there's a jwt token in the response
-            if (user.token) {
-                console.log(user);
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('user', JSON.stringify(user));
-                localStorage.setItem('user_name', username);
-                window.location.replace('/')
-            }
+            try{
+                if (user.token) {
+                    localStorage.setItem('user', JSON.stringify(user));
+                    localStorage.setItem('user_name', username);
+                    window.location.replace('/')
+                }
 
-            return user;
+                return user;
+            }catch(e){
+                console.log(e);
+                return null;
+            }
         });
 }
 
 function logout() {
-    // remove user from local storage to log user out
     localStorage.removeItem('user');
     window.location.replace('/#/login')
+}
+
+function addUser(userData){
+    const requestOptions = {
+        method: 'POST',
+        headers: authHeader(),
+        body: JSON.stringify(userData)
+    };
+    return fetch(`/api/users/user`, requestOptions).then(handleResponse);
+}
+
+function updateUser(userData){
+    const requestOptions = {
+        method: 'PUT',
+        headers: authHeader(),
+        body: JSON.stringify(userData)
+    };
+    return fetch(`/api/users/user`, requestOptions).then(handleResponse);
+}
+
+function deleteUser(user_id){
+    const requestOptions = {
+        method: 'DELETE',
+        headers: authHeader(),
+    };
+    return fetch(`/api/users/`+user_id, requestOptions).then(handleResponse);
 }
 
 function getUserByUsername(user_name){
@@ -57,15 +83,13 @@ function getAll() {
         headers: authHeader()
     };
 
-    return fetch(`/api/users`, requestOptions).then(handleResponse);
+    return fetch(`/api/users/all_users/`, requestOptions).then(handleResponse);
 }
 
 function handleResponse(response) {
-    return response.text().then(text => {
-        const data = text && JSON.parse(text);
         if (!response.ok) {
             if (response.status === 401) {
-                // auto logout if 401 response returned from api
+                // auto tokenExpired if 401 response returned from api
                 message.error("用户名或密码错误！");
                 logout();
             }else if(response.status === 500){
@@ -74,13 +98,23 @@ function handleResponse(response) {
                 message.error("用户名不存在！");
             }else if(response.status === 403){
                 message.error("无权限！");
+            }else if(response.status === 504){
+                message.error("504 Bad Gateway！");
+            }else{
+                message.error('HTTP '+response.status+' Error!');
             }
             //
             // const error = (data && data.message) || response.statusText;
             // return Promise.reject(error);
+            return null;
+        }else{
+            if(response.status === 201){
+                message.success("操作完成！")
+            }
+            return response.text().then(text => {
+                const data = text && JSON.parse(text);
+                return data;
+            });
 
         }
-
-        return data;
-    });
 }
