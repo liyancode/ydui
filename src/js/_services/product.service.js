f84409e4ced0de5bd64aadeadf0e517752b74435
liyancode <liyan.code@gmail.com>
2018-08-27 08:45:06 +0800

    product service

--- src/js/_services/product.service.js
+++ src/js/_services/product.service.js
@@ -1,4 +1,5 @@
-import { authHeader } from '../_helpers/auth-header';
+import { authHeader } from '../_helpers/authHeader';
+import { tokenExpired } from '../_helpers/tokenExpired';
 import {message} from 'antd'
 export const productService = {
     getAll,
@@ -61,30 +62,27 @@ function updateProduct(productData){
 }
 
 function handleResponse(response) {
-    return response.text().then(text => {
-        const data = text && JSON.parse(text);
-        if (!response.ok) {
-            if (response.status === 401) {
-                // auto logout if 401 response returned from api
-                message.error("登录信息过期，请重新登录！");
-                logout();
-            }else if(response.status === 500){
-                message.error("错误，请稍后再试！");
-            }else if(response.status === 404){
-                message.error("资源不存在！");
-            }else if(response.status === 403){
-                message.error("无权限！");
-            }else if(response.status === 409){
-                message.error("创建的记录已存在！");
-            }
-            //
-            // const error = (data && data.message) || response.statusText;
-            // return Promise.reject(error);
-        }else if(response.status === 201){
-            message.success("创建成功！");
-        }else{
-            // message.success("刷新成功！");
+    if (!response.ok) {
+        if (response.status === 401) {
+            // auto tokenExpired if 401 response returned from api
+            message.error("登录信息过期，请重新登录！");
+            tokenExpired();
+        }else if(response.status === 500){
+            message.error("错误，请稍后再试！");
+        }else if(response.status === 404){
+            message.error("资源不存在！");
+        }else if(response.status === 403){
+            message.error("无权限！");
+        }else if(response.status === 409){
+            message.error("创建的记录已存在！");
         }
-        return data;
-    });
+    // }else if(response.status === 201){
+    //     message.success("创建成功！");
+    }else{
+        // message.success("刷新成功！");
+        return response.text().then(text => {
+            const data = text && JSON.parse(text);
+            return data;
+        })
+    }
 }
