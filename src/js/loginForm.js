d0909e6a6e968e74b7561002645e60e1d571c4a9
liyancode <liyan.code@gmail.com>
2018-08-27 08:47:21 +0800

    loading status to login form

--- src/js/loginForm.js
+++ src/js/loginForm.js
@@ -1,28 +1,36 @@
 import React from 'react';
-import {Form, Icon, Input, Button, Checkbox} from 'antd';
+import {Form, Icon, Input, Button, Checkbox, Spin} from 'antd';
 
 import {userService} from "./_services/user.service"
-import WrappedHorizontalLoginForm from "./HorizontalLoginForm";
 
 const FormItem = Form.Item;
 
 class NormalLoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            login_result: 0,
+            user_name: '',
+        }
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
     componentDidMount() {
         // To disabled submit button at the beginning.
         this.props.form.validateFields();
     }
 
-    constructor(props) {
-        super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    };
-
     handleSubmit(e) {
         e.preventDefault();
         this.props.form.validateFields((err, values) => {
             if (!err) {
-                console.log('Received values of form: ', values);
-                userService.login(values["userName"], values["password"]);
+                this.setState({loading: true});
+                console.log('fdsafaaaaaa');
+                userService.login(values["userName"], values["password"]).then(data=>{
+                    this.setState({loading: false});
+                    console.log('fdsafaaaaaa2');
+                });
             }
         });
     }
@@ -30,36 +38,39 @@ class NormalLoginForm extends React.Component {
     render() {
         const {getFieldDecorator} = this.props.form;
         return (
-            <Form onSubmit={this.handleSubmit} className="login-form" style={{maxWidth: 320,padding:20,backgroundColor:'rgba(0, 0, 0, 0.25)'}}>
-                <FormItem>
-                    {getFieldDecorator('userName', {
-                        rules: [{required: true, message: '请输入你的用户名!'}],
-                    })(
-                        <Input prefix={<Icon type="user" style={{color: '#52c41a'}}/>} placeholder="用户名"/>
-                    )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator('password', {
-                        rules: [{required: true, message: '请输入你的密码！'}],
-                    })(
-                        <Input prefix={<Icon type="lock" style={{color: '#f5222d'}}/>} type="password"
-                               placeholder="密码"/>
-                    )}
-                </FormItem>
-                <FormItem>
-                    {getFieldDecorator('remember', {
-                        valuePropName: 'checked',
-                        initialValue: true,
-                    })(
-                        <Checkbox style={{color:'white'}}>记住我</Checkbox>
-                    )}
-                    <a className="login-form-forgot" href="" style={{float: 'right',color:'white'}}>忘记密码？</a>
-                    <Button type="primary" htmlType="submit" className="login-form-button" style={{width: '100%'}}>
-                        登录
-                    </Button>
-                    <a href="" style={{color:'white'}}>还没有账号？</a>
-                </FormItem>
-            </Form>
+            <Spin spinning={this.state.loading} tip="正在登录..." size="large">
+                <Form onSubmit={this.handleSubmit} className="login-form"
+                      style={{maxWidth: 320, padding: 20, backgroundColor: 'rgba(0, 0, 0, 0.25)'}}>
+                    <FormItem>
+                        {getFieldDecorator('userName', {
+                            rules: [{required: true, message: '请输入你的用户名!'}],
+                        })(
+                            <Input prefix={<Icon type="user" style={{color: '#52c41a'}}/>} placeholder="用户名"/>
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('password', {
+                            rules: [{required: true, message: '请输入你的密码！'}],
+                        })(
+                            <Input prefix={<Icon type="lock" style={{color: '#f5222d'}}/>} type="password"
+                                   placeholder="密码"/>
+                        )}
+                    </FormItem>
+                    <FormItem>
+                        {getFieldDecorator('remember', {
+                            valuePropName: 'checked',
+                            initialValue: true,
+                        })(
+                            <Checkbox style={{color: 'white'}}>记住我</Checkbox>
+                        )}
+                        <a className="login-form-forgot" href="" style={{float: 'right', color: 'white'}}>忘记密码？</a>
+                        <Button type="primary" htmlType="submit" className="login-form-button" style={{width: '100%'}}>
+                            登录
+                        </Button>
+                        <a href="" style={{color: 'white'}}>还没有账号？</a>
+                    </FormItem>
+                </Form>
+            </Spin>
         );
     }
 }
