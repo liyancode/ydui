6d4d690f0cd0e376b4ad77ee758f92576d77e07a
liyancode <liyan.code@gmail.com>
2018-08-28 21:23:34 +0800

    footer

--- src/js/_components/compnFooter.js
+++ src/js/_components/compnFooter.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import {Layout} from 'antd';
+import {Layout,Tag,Icon} from 'antd';
 const {Footer} = Layout;
 
 const CompnFooter = (props) => {
     return (
         <Footer style={{textAlign: 'center',background:'transparent',color:props.color}}>
-            ©2018 江苏耀迪新材料有限公司
+            <Icon type="copyright" /><span>2018 江苏耀迪新材料有限公司&nbsp;&nbsp;</span>
+            <Tag><Icon type="safety" style={{color:"#00ac47"}}/>耀迪·管理助手 版本v1.0</Tag>
         </Footer>
     );
 };
