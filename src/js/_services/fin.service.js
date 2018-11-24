a49ce618051e3c6db63daeb5d7bf7fd5ba87aec8
liyancode <liyan.code@gmail.com>
2018-11-24 11:05:04 +0800

    fin service

---
+++ src/js/_services/fin.service.js
@@ -0,0 +1,106 @@
+import { authHeader } from '../_helpers/authHeader';
+import {message} from 'antd'
+import {tokenExpired} from "../_helpers/tokenExpired";
+export const finService = {
+    getFinApprovalsByType,
+    getFinApprovalsByTypeAndResult,
+    getFinApprovalByFinApprovalID,
+    addOneFinApproval,
+    deleteOneFinApproval,
+    updateOneFinApproval,
+};
+
+function getFinApprovalsByType(type){
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`/api/fin/fin_approvals/type/`+type, requestOptions).then(handleResponse);
+}
+
+/**
+ * @param type: ask_price/contract
+ * @param result:waiting/pass/no
+ * @returns {Promise<Response>}
+ */
+function getFinApprovalsByTypeAndResult(type,result){
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`/api/fin/fin_approvals/type/`+type+`/result/`+result, requestOptions).then(handleResponse);
+}
+
+function getFinApprovalByFinApprovalID(fin_approval_id){
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`/api/fin/fin_approval/`+fin_approval_id, requestOptions).then(handleResponse);
+}
+
+function addOneFinApproval(fin_approval){
+    const requestOptions = {
+        method: 'POST',
+        headers: authHeader(),
+        body: JSON.stringify(fin_approval)
+    };
+    return fetch(`/api/fin/fin_approval`, requestOptions).then(handleResponse);
+}
+
+function updateOneFinApproval(fin_approval){
+    const requestOptions = {
+        method: 'PUT',
+        headers: authHeader(),
+        body: JSON.stringify(fin_approval)
+    };
+    return fetch(`/api/fin/fin_approval`, requestOptions).then(handleResponse);
+}
+
+
+function deleteOneFinApproval(fin_approval_id){
+    const requestOptions = {
+        method: 'DELETE',
+        headers: authHeader(),
+    };
+    return fetch(`/api/fin/fin_approval/`+fin_approval_id, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+    if (!response.ok) {
+        if (response.status === 401) {
+            // auto tokenExpired if 401 response returned from api
+            message.error("用户名或密码错误！");
+            tokenExpired();
+        }else if(response.status === 500){
+            message.error("错误，请稍后再试！");
+        }else if(response.status === 404){
+            message.error("记录不存在！");
+        }else if(response.status === 403){
+            message.error("无权限！");
+        }else if(response.status === 504){
+            message.error("504 Bad Gateway！");
+        }else{
+            message.error('HTTP '+response.status+' Error!');
+        }
+        //
+        // const error = (data && data.message) || response.statusText;
+        // return Promise.reject(error);
+        return null;
+    }else{
+        if(response.status===201){
+            message.success("操作完成！")
+        }
+        return response.text().then(text => {
+            let data = text && JSON.parse(text);
+            if(data==null){
+                data='ok'
+            }
+            return data;
+        });
+
+    }
+}
